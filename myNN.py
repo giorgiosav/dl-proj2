8b4d3e91674f366d7d6568d8d1bfa968671c3045
manuleo <manuel.leone97@gmail.com>
2020-04-26 12:09:14 +0200

    First implementation

---
+++ myNN.py
@@ -0,0 +1,78 @@
+import torch
+
+
+class Module(object):
+    def forward(self, *input_val):
+        raise NotImplementedError
+
+    def backward(self, *gradwrtoutput):
+        raise NotImplementedError
+
+    def param(self):
+        return []
+
+
+class Linear(Module):
+
+    def __init__(self, nb_hidden1: int, nb_hidden2: int):
+        self.weights: torch.Tensor = torch.empty(nb_hidden2, nb_hidden1).normal_()
+        self.bias: torch.Tensor = torch.empty(nb_hidden2).normal_()
+        self.dweights: torch.Tensor = torch.empty(nb_hidden2, nb_hidden1)
+        self.dbias: torch.Tensor = torch.empty(nb_hidden2)
+        self.input: torch.Tensor = torch.empty(nb_hidden1)
+
+    def forward(self, input_val: torch.Tensor) -> torch.Tensor:
+        """
+        :param input_val:
+        :return:
+        """
+        self.input = input_val
+        return self.weights @ input_val + self.bias
+
+    def backward(self, gradwrtoutput: torch.Tensor) -> torch.Tensor:
+        dx = self.weights.t() @ gradwrtoutput
+        self.dbias.add_(gradwrtoutput)
+        self.dweights.add_(gradwrtoutput.view((-1, 1)).mm(self.input.view(1, -1)))
+        return dx
+
+    def param(self) -> list:
+        return [(self.weights, self.dweights), (self.bias, self.dbias)]
+
+
+class ReLU(Module):
+
+    def __init__(self):
+        self.input: torch.Tensor = torch.empty(0)
+        self.drelu: torch.Tensor = torch.empty(0)
+
+    def forward(self, input_val: torch.Tensor) -> torch.Tensor:
+        self.input = input_val
+        input_val = input_val.view((-1, 1))
+        zeros = torch.zeros_like(input_val)
+        conc = torch.cat((input_val, zeros), 1)
+        act = conc.max(1)
+        self.drelu = (1 - act[1]).float()
+        return act[0]
+
+    def backward(self, gradwrtoutput: torch.Tensor) -> torch.Tensor:
+        return self.drelu * gradwrtoutput
+
+    def param(self):
+        return []
+
+
+class Tanh(Module):
+
+    def __init__(self):
+        self.input: torch.Tensor = torch.empty(0)
+
+    def forward(self, input_val):
+        self.input = input_val
+        return torch.tanh(input_val)
+
+    def backward(self, gradwrtoutput):
+        dtanh = 1 - torch.pow(torch.tanh(self.input), 2)
+        return dtanh * gradwrtoutput
+
+    def param(self):
+        return []
--- test.py
+++ test.py
@@ -1,7 +1,26 @@
-
+import torch
+import myNN
 
 def main():
-    pass
+    torch.set_grad_enabled(False)
+    # linear = myNN.Linear(4, 5)
+    # out = linear.forward(torch.Tensor([1,1,1,1]))
+    # back = linear.backward(torch.Tensor([1,1,1,1,1]))
+    # for p in linear.param():
+    #     for p1 in p:
+    #         print(p1)
+    # print(out)
+    # print(back)
+
+    # relu = myNN.ReLU()
+    # print(relu.forward(torch.Tensor([-1, -2, 0, 1, 2])))
+    # print(relu.backward(torch.Tensor([1,1,1,2,1])))
+
+    relu = myNN.Tanh()
+    print(relu.forward(torch.Tensor([-1, -2, 0, 1, 2])))
+    print(relu.backward(torch.Tensor([1,1,1,2,1])))
+
+    return
 
 
 if __name__ == '__main__':
